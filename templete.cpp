74d5ecfcef7b9b68af19c754bfc5963c38d0c177
simauma1203 <minecrafter12031203@gmail.com>
2018-03-21 19:22:30 +0900

    Update templete.cpp
    update addition

--- templete.cpp
+++ templete.cpp
@@ -1,18 +1,32 @@
 //#include <bits/stdc++.h>
 #include<iostream>
+#include<cstdio>
 #include<string>
-#include<queue>
+#include<algorithm>
 #include<vector>
-#define FOR(i,a,b) for(int i=(a);i<(b);i++)//初期値a,i<bの範囲でループ
-#define REP(i,n) FOR(i,0,n)//初期値0,i<nの範囲でループ
+#include<stack>
+#include<queue>
+#define FOR(i,a,b) for(int i=(a);i<(b);i++)
+#define REP(i,n) FOR(i,0,n)
 using namespace std;
 typedef long long ll;//int64
 typedef unsigned long long ull;
+int gcd(int a,int b){return b?gcd(b,a%b):a;}
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
+//----------program from here----------
+
+
 
 int main(){
 
-
     
+    
+    
+
+
+
+
 
     return 0;
 }
